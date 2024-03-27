8452392f3ff49ee66c256c80d64dc392f8a1becd
rayen445 <nesstinebacc@gmail.com>
2024-03-28 00:06:20 +0100

    commit

--- _printf.c
+++ _printf.c
@@ -8,8 +8,48 @@
  */
 int _printf(const char *format, ...)
 {
-    /* Implementation of _printf function */
+    va_list args;
     int count = 0;
-    // Implement your code here
+
+    va_start(args, format);
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    count += _putchar(va_arg(args, int));
+                    break;
+                case 's':
+                    {
+                        char *str = va_arg(args, char *);
+                        if (str == NULL)
+                            str = "(null)";
+                        while (*str != '\0')
+                        {
+                            count += _putchar(*str);
+                            str++;
+                        }
+                        break;
+                    }
+                case '%':
+                    count += _putchar('%');
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            count += _putchar(*format);
+        }
+        format++;
+    }
+
+    va_end(args);
+
     return count;
 }
