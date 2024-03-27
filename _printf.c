eb5dc35ac0d1ec55402dbe414a0684752f6fec0c
rayen445 <nesstinebacc@gmail.com>
2024-03-28 00:38:28 +0100

    commit

--- _printf.c
+++ _printf.c
@@ -17,9 +17,6 @@ int _printf(const char *format, ...)
     {
         if (*format == '%')
         {
-            count += _putchar('%');
-        }
-        {
             format++;
             switch (*format)
             {
